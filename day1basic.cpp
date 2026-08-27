46ac60b6330125b9ea9fd7bff0783a91fc5343f0
Mahi jain <jainaryan976@gmail.com>
2026-08-27 09:48:44 +0530

    Add files via upload

---
+++ day1basic.cpp
@@ -0,0 +1,90 @@
+#include <stdio.h>
+
+int main()
+{
+//question 1 ---> area of square
+// int s;
+// int area;
+// printf("enter sides\n");
+// scanf("%d",&s);
+// area = s*s;
+// printf("area is = %d",area);
+//pseudocode of Q1
+// input side and area
+// print side
+// processor area= side*side
+// print area
+//exit
+
+//find min of two number 
+// int a,b;
+// printf("enter two numbers\n");
+// scanf("%d %d", &a , &b);
+// if(a>b){
+//     printf("\n%d",a);
+// }
+// else{
+//     printf("\n%d",b);
+// }
+//pseudocode of Q2
+// input a and b 
+// print a and b 
+// if a>b
+// print a 
+// else 
+// print b 
+// exit
+
+
+//find odd even
+// int a;
+// printf("enter number\n");
+// scanf("%d",&a);
+// if(a%2 ==0){
+//     printf("number is even");
+// }
+// else{
+//     printf("number is odd");
+// }
+// pseudocode of Q3
+// intput n 
+// if n%2 ==0
+//   even
+// else 
+//   odd
+// exit   
+
+//sum of N numbers
+// int n;
+// int count =1;
+// int sum = 0;
+// printf("enter N\n");
+// scanf("%d",&n);
+// while (count <=n){
+//     sum = sum+count;
+//     count++;
+// }
+// printf("sum is %d",sum);
+
+// pseudocode of question 4
+// input N
+// count =1 ; sum = 0
+// while count <= N
+//       sum = sum + count
+//       count ++
+// print sum 
+// exit
+
+// pseudocode to find prime or not
+// input n 
+// i = 2 
+// while i <= (n-1)
+//     i n%i ==0
+// print non prime
+// exit
+// else 
+// i+=1
+// print prime
+// exit
+    return 0;
+}
