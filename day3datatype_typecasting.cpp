242b19251c6a4efcc2252e02701d6f9d1ac34d2d
Mahi jain <jainaryan976@gmail.com>
2026-08-29 07:15:30 +0530

    Add files via upload

---
+++ day3datatype_typecasting.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    //boilerplate code 
+//     #include <iostream>
+// using namespace std;
+
+// int main (){
+
+//     return 0;
+// } 
+
+//variables---> a,b etc 
+//data type --> int(4 bytes)(total = n*4),char(1 byte) ,float(4byte) , bool(1/0) ,double(8)
+// all these are primitive data types
+// int age = 6;
+// cout<< sizeof(age) <<endl;
+// char c = 'm';
+// cout << sizeof(c) <<endl;
+// float f = 11.24;
+// cout << sizeof(f)<<endl;
+// bool issafe = true;
+// cout << issafe << endl;
+// bool isnot = false;
+// cout << isnot << endl;
+// double price = 1.64;
+// cout << sizeof(double)<<endl;
+
+//TYPECASTING
+//converting data from 1 type to another
+//impiclit ---> smaller bytes to bigger bytes --->  called conversion
+//explicit ----> bigger bytes to smaller ---> called casting
+// char grade = 'A';
+// int value = grade;
+// cout << value;
+
+// double price = 100.99;
+// int newprice = (int)price;
+// cout<<newprice;
+    return 0;
+}
