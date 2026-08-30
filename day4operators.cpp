4e8a6670e90acbb04b02e90e44052d7085b9d2e4
Mahi jain <jainaryan976@gmail.com>
2026-08-30 22:03:27 +0530

    Add files via upload

---
+++ day4operators.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main (){
+//How to take input in c++
+// int age ;
+// cout<<"enter the age"<<"\n";
+// cin>>age;
+
+// cout <<"Your age is : "<< age << endl;
+
+//operators --> arithemetic , relational , logical
+// arithmetic --> +,-,*,/
+
+// int a = 10;
+// int b = 2;
+// int sum = a+b;
+// cout<< sum << endl;
+
+// int a = 10;
+// int b = 2;
+// int diff = a-b;
+// cout<< diff << endl;
+
+// int a = 11;
+// int b = 2;
+// int div = a/b;
+// cout<< div << endl;
+
+// cout<< (5/(double)2)<<endl; // typecasting
+// cout<<(5/(float)2)<<endl;
+
+//relational ---> <,<=,>,>= ,==,!=
+// cout<<(3>5)<<endl;//as this statement is false output will come 0
+// cout<< (3<5)<<endl; // output = 1 as this statement is true
+
+
+//logical --> OR (||),AND (&&),not(!)
+// cout<<!(3>5)<<endl; // this is true as it say 3>5 not true
+// cout<< !(3<5)<<endl;// false as it says not before 3<5
+// cout<< ((3>5) || (3<5)) <<endl;//if either of the condition is true ans will come true in OR
+// cout <<( (3>5) && (3<5)) <<endl; // if either of the condition is false ans will come 0 for false in AND
+   return 0;
+}
