895a74f2430eb6d529de8a1d365c445e0e4b6fe0
Mahi jain <jainaryan976@gmail.com>
2026-08-31 08:52:15 +0530

    Add files via upload

---
+++ day5ifelse.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+//conditional statements
+//if-else 
+// int n;
+// cout<<"Enter a number"<<"\n";
+// cin>> n;
+
+// if (n >=0){
+//     cout<<"POSITIVE";
+// }
+// else{
+//     cout<<"Negative";
+// }
+
+//voting app
+// int age;
+// cout<<"Enter age"<<"\n";
+// cin>>age;
+// if(age>=18){
+//     cout<<"Eligible for voting";
+// }
+// else{
+//     cout<<"Not eligible for voting";
+// }
+
+//even odd 
+// int n;
+// cout<<"enter a number"<<"\n";
+// cin>>n;
+// if(n%2==0){
+//     cout<<"even";
+// }
+// else{
+//     cout<<"odd";
+// }
+
+//grading system 
+// int marks;
+// cout << "enter marks out of 100"<<"\n";
+// cin>>marks;
+// if((marks>10) && (marks<30)){
+//     cout<<"D grade";
+// }
+// else if ((marks>30) && (marks<50)){
+//     cout<<"C grade";
+// }
+// else if ((marks>50) && (marks<85)){
+//     cout<<"B grade";
+// }
+// else{
+//     cout<<"A grade";
+// }
+
+//find character lowercae or uppercase
+//we should know that a-z --->97-122
+//and A - Z ---> 65 -90
+// char ch;
+// cout<< "Enter character"<<"\n";
+// cin >> ch;
+// if((ch>=65) &&  (ch<=90)){
+//     cout <<"Uppercase";
+// }
+// else if((ch>=97) && (ch<=122)){
+//     cout<<"Lowercase";
+// }
+
+    return 0;
+}
