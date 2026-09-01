ff93e5c006ff9af5c02c2ba0acdbfd5a74784f0c
Mahi jain <jainaryan976@gmail.com>
2026-09-01 09:26:09 +0530

    Add files via upload

---
+++ day6loops.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+//ternary statements
+// int n ;
+// cout<< "enter n"<<"\n";
+// cin>>n;
+// cout << (n%2==0 ? "even" : "odd")<<endl;
+
+//LOOPS
+//1) WHILE LOOPS ---- jab tak 
+//question --> print numbers from 1 to 10
+// int n= 1;
+// while (n<=10){
+//     cout<< n;
+//     n++;
+//     cout <<"\n";
+// }
+
+// 2) FOR LOOPS  only syntax is different 
+// int n;
+// int i = 1;
+// cout<<"enter n"<<"\n";
+// cin>> n;
+// for(i = 1; i<= n ; i++){
+//     cout << i<< endl;
+// }
+
+// sum of numbers from 1 to n 
+// int n;
+// int sum = 0;
+// cout << "Enter n"<< "\n";
+// cin >> n;
+// for (int i =0 ; i<=n ; i++){
+//     sum = sum + i;
+//     cout <<" sum =  " << sum << endl;
+// }
+
+//sum of all odd numbers from 1 to n
+// int n;
+// int sum=0;
+// cout << "Enter n"<<"\n";
+// cin>> n;
+// for (int i = 1; i<=n ; i++){
+//     if(i%2 != 0){
+//         sum = sum + i;
+//     }   
+// }
+// cout << "sum of odd numbers is  "<< sum << endl;
+
+//do while loops
+// while (3 >5){ // while loop mai it will stop once the loop fails
+//     cout <<"hello world"<<"\n";
+// }
+
+// do{
+//     cout <<"hello world"<<"\n";
+// }while (3>5);
+// do whuile mai condition baad m check hogi ek baar kaam hojyega 
+
+//check if number is prime or not
+// int n;
+// cout << "Enter number"<<"\n";
+// cin>> n;
+// for (int i = 2 ; i <=n ; i++){
+//     if (n%i==0){
+//         cout << "Not a prime number";
+//     }
+//     else {
+//         cout << "Prime number";
+//     }
+//     break;
+// }
+// int n;
+// bool isprime = true;
+// cout << "Enter number"<<"\n";
+// cin>> n;
+// for (int i = 2 ; i<= n-1; i++){
+//     if ( n%i==0){
+//         isprime = false;
+//         break;
+//     }
+// }
+// if (isprime == true){
+//     cout <<"PRIME NUMBER\n";
+// }
+// else {
+//     cout << "NOT A PRIME NUMBER\n";
+// }
+    return 0;
+}
