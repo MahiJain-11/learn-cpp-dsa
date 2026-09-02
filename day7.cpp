ba62305de9c5f9a84c1e6368c857e28e1252a388
Mahi jain <jainaryan976@gmail.com>
2026-09-02 21:27:43 +0530

    Add files via upload

---
+++ day7.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+//patterns
+//1) nested loops
+// int n;
+// cout << "enter number of rows "<<"\n";
+// cin >> n;
+// for (int i = 1 ; i <=n ; i++){
+//     for (int j = 1 ; j <=n ; j++){
+//     cout << "* ";
+//     }
+//     cout <<"\n"<<endl;
+// }    
+
+//2)square patterns
+// int n ;
+// cout << "enter the number of rows and cols you want"<<"\n";
+// cin >> n;
+// for (int i = 1 ; i<=n ; i++){
+//     for (int j =1; j<=n ; j++){
+//         cout << j;
+//     }
+//     cout << "\n"<< endl;
+// }
+
+// int n;
+// int num = 1;
+// cout<< "enter nnumber of rows"<<"\n";
+// cin >> n;
+// for (int i =0 ; i <=n ; i++){
+//     for (int j = 0 ; j <n ; j++){
+//         cout << num << " ";
+//         num ++;
+// }
+// cout << endl;
+// }
+
+//triangle pattern
+// int n;
+// cout << "enter n"<<"\n";
+// cin >> n;
+// for (int i = 0 ; i <=n ; i++){
+//     for (int j = 0 ; j<=i+1 ; j++){
+//         cout << "*";
+//     }
+//     cout << "\n"<< endl;
+// }
+    return 0;
+}
