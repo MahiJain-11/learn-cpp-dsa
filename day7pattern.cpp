5e40149b6334377598d891778e5e6c59b7e3c255
Mahi jain <jainaryan976@gmail.com>
2026-09-03 11:34:08 +0530

    Add files via upload

---
+++ day7pattern.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+//patterns
+//1) nested loops
+// int n;
+// cout << "enter number of rows "<<"\n";
+// cin >> n;
+// for (int i = 1 ; i <=n ; i++){
+//     for (int j = 1 ; j <=n ; j++){
+//     cout << "* ";
+//     }
+//     cout <<"\n"<<endl;
+// }    
+
+//2)square patterns
+// int n ;
+// cout << "enter the number of rows and cols you want"<<"\n";
+// cin >> n;
+// for (int i = 1; i<=n ; i++){
+//     for (int j =1; j<=n ; j++){
+//         cout << j;
+//     }
+//     cout << "\n"<< endl;
+// }
+//1-2-3-4 pattern 
+// int n;
+// int num = 1;
+// cout<< "enter nnumber of rows"<<"\n";
+// cin >> n;
+// for (int i =0 ; i <=n ; i++){
+//     for (int j = 0 ; j <n ; j++){
+//         cout << num <<" " ;
+//         num ++;
+// }
+// cout << endl;
+// }
+
+//triangle pattern
+// int n;
+// cout << "enter n"<<"\n";
+// cin >> n;
+// for (int i = 0; i <=n ; i++){
+//     for (int j = 0 ; j<=i+1 ; j++){
+//         cout << "*";
+//     }
+//     cout << "\n"<<endl;
+// }
+
+//number triangle pattern 
+// int n;
+// cout << "enter n" ; 
+// cin >>n;
+// for (int i = 0 ; i<n ; i++){
+//     for (int j = 0 ; j< i+1 ; j++){
+//         cout << (i+1);
+//     }
+//     cout<< endl;
+// }
+
+//character triangle pattern
+
+// for(int i = 65 ; i<70; i++){
+//     for (int j = 65 ;j<=i ; j++){
+//         cout << (char)i;
+//     }
+//     cout << endl;
+// }
+
+// int n ; 
+// cout << "enter n"<<"\n";
+// cin >> n;
+// for(int i = 0 ;i<=n ; i++){
+//     for (int j = 1; j<= i+1 ; j++){
+//         cout << j << " ";
+//     }
+//     cout << endl;
+// }
+
+// reverse triangle pattern
+// int n;
+// cout << "Enter n"<< "\n";
+// cin >> n;
+// for (int i = 0 ; i <= n ;i++){
+//     for (int j = i+1 ; j>=1 ; j--){
+//         cout << j<< " ";
+//     }
+//     cout << endl;
+// }
+
+// floyds triangle pattern
+// int n ; 
+// int a = 0;
+// cout << "enter n ";
+// cin >> n;
+// for (int i = 0 ; i<=n ; i++){
+//     for (int j =1; j<=i+1; j++){
+//         cout << " ";
+//         cout << ++a ;
+//     }
+//     cout << endl;
+// }
+
+// inverted triangle pattern
+// int n = 4;
+// int num = 1;
+
+// for(int i = 0 ; i<n  ; i++){
+//     for ( int j =0 ; j<i ; j++){
+//         cout << " ";
+//     }
+
+//     for (int j = 0 ; j <= n-i ; j++){
+//         cout << (i+1)<< " ";
+//     }
+//     cout << endl;
+// }
+    return 0;
+}
