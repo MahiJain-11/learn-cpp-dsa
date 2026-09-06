3b302c10707523c132eea89e9ac757b18b63d65d
Mahi jain <jainaryan976@gmail.com>
2026-09-06 09:44:34 +0530

    Add files via upload

--- day9questionfunction.cpp
+++ day9questionfunction.cpp
@@ -1,30 +1,61 @@
 #include <iostream>
 using namespace std;
 //Q1 
-int sumofdigits(int num){
-    int digsum = 0;
-    while (num >0){
-       int  lastdig = num %10;
-       num = num /10;
-       digsum = digsum +lastdig;
+// int sumofdigits(int num){
+//     int digsum = 0;
+//     while (num >0){
+//        int  lastdig = num %10;
+//        num = num /10;
+//        digsum = digsum +lastdig;
 
+//     }
+//     return digsum;
+
+// }
+//  Q2 nCr
+// int factorial(int n) {
+//     int fact = 1;
+
+//     for (int i = 1; i <= n; i++) {
+//         fact = fact * i;
+//     }
+
+//     return fact;
+// }
+
+// int nCr(int n, int r) {
+//     return factorial(n) / (factorial(r) * factorial(n - r));
+// }
+
+// Q3 prime or not
+// void  isprime (int n){
+//     for(int i = 2 ; i <= n ; i++){
+//     if (n % i == 0){
+//         cout << "NOT PRIME";
+//         break;
+//     }
+//     else {
+//         cout << "PRIME";
+//         break;
+//     }
+//     }
+// }
+
+//Q4 print prime number
+void printprime(int n){
+    for(int i = 2 ; i <=n ; i++){
+    int num =0;
+    
+    for(int j = 1; j<=i ; j++){
+        if(i%j==0){
+            num++;
+        }
     }
-    return digsum;
-
-}
-// nCr
-int factorial(int n) {
-    int fact = 1;
-
-    for (int i = 1; i <= n; i++) {
-        fact = fact * i;
+    if(num == 2){
+        cout << i <<" ";
     }
-
-    return fact;
-}
-
-int nCr(int n, int r) {
-    return factorial(n) / (factorial(r) * factorial(n - r));
+    
+    }
 }
 int main (){
 //stack = stores data ---> static allocation
@@ -35,11 +66,23 @@ int main (){
 // cout << "SUM IS = " << sumofdigits(99)<<endl;
 
 //calculate nCr binomial coefficient for n and r
-  int n, r;
+//   int n, r;
 
-    cout << "Enter n and r: ";
-    cin >> n >> r;
+//     cout << "Enter n and r: ";
+//     cin >> n >> r;
 
-    cout << "nCr = " << nCr(n, r);
+//     cout << "nCr = " << nCr(n, r);
+
+//WAF to check if number is prime or not 
+// int n;
+// cout << "enter n"<<"\n";
+// cin >> n;
+// isprime(n);
+
+//WAF to print all prime number from 2 TO N
+int n;
+cout <<"ENTER N ";
+cin >> n;
+printprime(n);
     return 0;
 }
