f6c27eb6fb59eecb1285246a42d655b71ff9a44a
Mahi jain <jainaryan976@gmail.com>
2026-09-08 19:51:54 +0530

    Add files via upload

---
+++ day11binarysystem.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+//decimal to binary
+//     int n;
+//     cout << "Enter decimal number: ";
+//     cin >> n;
+
+//     int binary[32];
+//     int i = 0;
+
+//     while (n > 0) {
+//         binary[i] = n % 2;
+//         n = n / 2;
+//         i++;
+//     }
+// cout << "Binary = ";
+//     for (i = i - 1; i >= 0; i--) {
+//         cout << binary[i];
+//     }
+
+// binary to decimal 
+// int n;
+//     cout << "Enter binary number: ";
+//     cin >> n;
+
+//     int decimal = 0;
+//     int power = 1;
+
+//     while (n > 0) {
+//         int digit = n % 10;
+//         decimal = decimal + digit * power;
+//         power = power * 2;
+//         n = n / 10;
+//     }
+
+//     cout << "Decimal = " << decimal;
+
+
+
+    return 0;
+}
