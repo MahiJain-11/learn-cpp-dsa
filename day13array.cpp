f9d9e07ee2bc795065ba5a9339e91d79e7febe3f
Mahi jain <jainaryan976@gmail.com>
2026-09-12 12:58:21 +0530

    Add files via upload

---
+++ day13array.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+//arrays
+// int arr[5] = {1,2,3,4,5};
+// for(int i = 0 ; i < 5 ; i++){
+// cout << arr[i];
+// }
+
+// find smalles and largest element in array
+// smallest
+// int arr[10] = {-111,4,5,2,6,7,11,12,116,35};
+// int min = arr[0];
+// for(int i =0 ; i <=9 ; i++){
+//     if (min > arr[i]){
+//         min = arr[i];
+//     }
+// }
+//  cout <<"smallest element is = "<< min << endl;
+
+//largest 
+// int arr[10] = {-111,4,5,2,6,7,11,12,116,35};
+// int max = arr[0];
+// for(int i = 0 ; i<= 9 ; i++){
+//     if(max < arr[i]){
+//         max = arr[i];
+//     }
+// }
+// cout << "Largest element is = "<< max <<endl;
+
+//Linear search 
+
+    return 0;
+}
