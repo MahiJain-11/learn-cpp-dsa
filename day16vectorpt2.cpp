78636096ee7045b335e1f4666880cf9dcfa28235
Mahi jain <jainaryan976@gmail.com>
2026-09-17 17:11:21 +0530

    Add files via upload

---
+++ day16vectorpt2.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+//vector functions 
+//1 ) SIZE    
+// vector<char>vec = {'a','b','c','d','e','f'};
+// cout << "size =  " << vec.size()<<endl;
+// for(char val : vec){
+//     cout << val << endl;
+// }
+//Push_back
+// vector<int>vec;
+// cout <<"Size = "<<vec.size()<<endl;
+// vec.push_back(25);
+// vec.push_back(40);
+// vec.push_back(50);
+// cout<<"Size after pushback = "<<vec.size()<<endl;
+// for(int val : vec){
+//     cout << val <<endl;
+// }
+
+//pop_back
+// vector<int>vec;
+// cout <<"Size = "<<vec.size()<<endl;
+// vec.push_back(25);
+// vec.push_back(40);
+// vec.push_back(50);
+// cout<<"Size after pushback = "<<vec.size()<<endl;
+// vec.pop_back();//last index vli value hat jyegi
+// cout<<"Size after popback = "<<vec.size()<<endl;
+// for(int val : vec){
+//     cout << val <<endl;
+// }
+
+//front and back
+// vector<int>vec;
+// cout <<"Size = "<<vec.size()<<endl;
+// vec.push_back(25);
+// vec.push_back(40);
+// vec.push_back(50);
+// cout<<"Size after pushback = "<<vec.size()<<endl;
+// vec.pop_back();//last index vli value hat jyegi
+// cout<<"Size after popback = "<<vec.size()<<endl;
+// cout << vec.front()<<endl;
+// cout << vec.back()<<endl;
+
+//at
+// vector<int>vec;
+// vec.push_back(25);
+// vec.push_back(40);
+// vec.push_back(50);
+// cout << vec.at(2)<<endl;
+
+//static and dynamic allocation
+// static memory allocate during complie time i.e. before the programme startes running
+//arrays use this
+//size is not fixed
+//it uses stack memory
+//static is faster  and simpler but less flexible
+//dynamic memory allocation  during run time 
+//vectors use dynamic memory allocation
+//size is not fixed
+//it uses heap memory
+//provides the resizing capability necessary for modern, data-intensive application
+    return 0;
+}
